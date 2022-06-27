e4da97881ee4010d73c5d066eb1d0cbbfdc7337e
Rina Fujino <18257209+rina23q@users.noreply.github.com>
2022-06-27 15:41:02 +0200

    Build improve
    Signed-off-by: Rina Fujino <18257209+rina23q@users.noreply.github.com>

--- ci/build/build_for_arm.sh
+++ ci/build/build_for_arm.sh
@@ -6,6 +6,8 @@ ARCH=$1
 
 cargo install cargo-deb --version 1.38.1
 cargo install cargo-strip
+
+
 cross build --release --target="$ARCH"
 
 # armv7 uses `arm-linux-gnueabihf-strip`; aarch64 uses `aarch64-linux-gnu-strip`
@@ -13,7 +15,7 @@ cross build --release --target="$ARCH"
 sudo apt update
 sudo apt-get --assume-yes install binutils-arm-linux-gnueabihf binutils-aarch64-linux-gnu
 
-# Strip
+# Strip for release artifacts
 arm-linux-gnueabihf-strip target/"$ARCH"/release/tedge || aarch64-linux-gnu-strip target/"$ARCH"/release/tedge
 arm-linux-gnueabihf-strip target/"$ARCH"/release/tedge_mapper || aarch64-linux-gnu-strip target/"$ARCH"/release/tedge_mapper
 arm-linux-gnueabihf-strip target/"$ARCH"/release/tedge_agent || aarch64-linux-gnu-strip target/"$ARCH"/release/tedge_agent
@@ -23,13 +25,18 @@ arm-linux-gnueabihf-strip target/"$ARCH"/release/tedge_apama_plugin || aarch64-l
 arm-linux-gnueabihf-strip target/"$ARCH"/release/c8y_log_plugin || aarch64-linux-gnu-strip target/"$ARCH"/release/c8y_log_plugin
 arm-linux-gnueabihf-strip target/"$ARCH"/release/c8y_configuration_plugin || aarch64-linux-gnu-strip target/"$ARCH"/release/c8y_configuration_plugin
 
-cargo deb -p tedge
-cargo deb -p tedge_mapper
-cargo deb -p tedge_apt_plugin
-cargo deb -p tedge_apama_plugin
-cargo deb -p tedge_agent
-cargo deb -p tedge_watchdog
-cargo deb -p c8y_log_plugin
-cargo deb -p c8y_configuration_plugin
-cargo build --release -p sawtooth_publisher
-cargo build --release -p tedge_dummy_plugin
+cargo deb -p tedge --no-strip --no-build --target="$ARCH"
+cargo deb -p tedge_mapper --no-strip --no-build --target="$ARCH"
+cargo deb -p tedge_apt_plugin --no-strip --no-build --target="$ARCH"
+cargo deb -p tedge_apama_plugin --no-strip --no-build --target="$ARCH"
+cargo deb -p tedge_agent --no-strip --no-build --target="$ARCH"
+cargo deb -p tedge_watchdog --no-strip --no-build --target="$ARCH"
+cargo deb -p c8y_log_plugin --no-strip --no-build --target="$ARCH"
+cargo deb -p c8y_configuration_plugin --no-strip --no-build --target="$ARCH"
+
+# Test artifacts
+cargo build --release -p sawtooth_publisher --target="$ARCH"
+cargo build --release -p tedge_dummy_plugin  --target="$ARCH"
+
+arm-linux-gnueabihf-strip target/"$ARCH"/release/sawtooth_publisher || aarch64-linux-gnu-strip target/"$ARCH"/release/sawtooth_publisher
+arm-linux-gnueabihf-strip target/"$ARCH"/release/tedge_dummy_plugin || aarch64-linux-gnu-strip target/"$ARCH"/release/tedge_dummy_plugin
