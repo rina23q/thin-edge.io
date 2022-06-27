084e83e1da4534853766b45871ce74500294e71e
Rina Fujino <18257209+rina23q@users.noreply.github.com>
2022-06-27 21:41:03 +0200

    cargo test features for amd64
    Signed-off-by: Rina Fujino <18257209+rina23q@users.noreply.github.com>

---
+++ ci/integration-test/run-cargo-test-for-amd64.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+# Compile in advance to avoid that cargo compiles during the test run
+# this seems to have an impact on some tests as the timing differs
+cargo test --verbose --no-run --features integration-test
+
+# To run the test for features here is kind of experimental
+# they could fail if GitHub blocks external connections.
+# It seems like they rarely do.
+cargo test --verbose --features integration-test,requires-sudo -- \
+--skip sending_and_receiving_a_message
