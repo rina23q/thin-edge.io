d6d28feaba0c0cc90587e9c9b1f97f8c3dbae688
Rina Fujino <18257209+rina23q@users.noreply.github.com>
2022-06-27 18:34:37 +0200

    cargo install by workflow
    Signed-off-by: Rina Fujino <18257209+rina23q@users.noreply.github.com>

--- ci/build/build_for_arm.sh
+++ ci/build/build_for_arm.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 ARCH=$1
 
-cargo install cargo-deb --version 1.38.1
-cargo install cargo-strip
+#cargo install cargo-deb --version 1.38.1
+#cargo install cargo-strip
 
-
+# build cross release for target
 cross build --release --target="$ARCH"
 
 # armv7 uses `arm-linux-gnueabihf-strip`; aarch64 uses `aarch64-linux-gnu-strip`
@@ -15,7 +15,7 @@ cross build --release --target="$ARCH"
 sudo apt update
 sudo apt-get --assume-yes install binutils-arm-linux-gnueabihf binutils-aarch64-linux-gnu
 
-# Strip for release artifacts
+# strip for release artifacts
 arm-linux-gnueabihf-strip target/"$ARCH"/release/tedge || aarch64-linux-gnu-strip target/"$ARCH"/release/tedge
 arm-linux-gnueabihf-strip target/"$ARCH"/release/tedge_mapper || aarch64-linux-gnu-strip target/"$ARCH"/release/tedge_mapper
 arm-linux-gnueabihf-strip target/"$ARCH"/release/tedge_agent || aarch64-linux-gnu-strip target/"$ARCH"/release/tedge_agent
@@ -25,6 +25,7 @@ arm-linux-gnueabihf-strip target/"$ARCH"/release/tedge_apama_plugin || aarch64-l
 arm-linux-gnueabihf-strip target/"$ARCH"/release/c8y_log_plugin || aarch64-linux-gnu-strip target/"$ARCH"/release/c8y_log_plugin
 arm-linux-gnueabihf-strip target/"$ARCH"/release/c8y_configuration_plugin || aarch64-linux-gnu-strip target/"$ARCH"/release/c8y_configuration_plugin
 
+# build debian packages for target
 cargo deb -p tedge --no-strip --no-build --target="$ARCH"
 cargo deb -p tedge_mapper --no-strip --no-build --target="$ARCH"
 cargo deb -p tedge_apt_plugin --no-strip --no-build --target="$ARCH"
@@ -34,9 +35,10 @@ cargo deb -p tedge_watchdog --no-strip --no-build --target="$ARCH"
 cargo deb -p c8y_log_plugin --no-strip --no-build --target="$ARCH"
 cargo deb -p c8y_configuration_plugin --no-strip --no-build --target="$ARCH"
 
-# Test artifacts
+# build binaries for testing
 cross build --release -p sawtooth_publisher --target="$ARCH"
 cross build --release -p tedge_dummy_plugin  --target="$ARCH"
 
+# strip binaries for testing
 arm-linux-gnueabihf-strip target/"$ARCH"/release/sawtooth_publisher || aarch64-linux-gnu-strip target/"$ARCH"/release/sawtooth_publisher
 arm-linux-gnueabihf-strip target/"$ARCH"/release/tedge_dummy_plugin || aarch64-linux-gnu-strip target/"$ARCH"/release/tedge_dummy_plugin
