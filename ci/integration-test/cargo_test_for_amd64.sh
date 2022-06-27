f4454e98da524695e82901b50deee14e60054bb2
Rina Fujino <18257209+rina23q@users.noreply.github.com>
2022-06-27 22:59:42 +0200

    install for arm
    Signed-off-by: Rina Fujino <18257209+rina23q@users.noreply.github.com>

--- ci/integration-test/cargo_test_for_amd64.sh
+++ ci/integration-test/cargo_test_for_amd64.sh
@@ -6,6 +6,8 @@ set -euo pipefail
 # this seems to have an impact on some tests as the timing differs
 cargo test --verbose --no-run --features integration-test
 
+cargo build -p tedge_dummy_plugin
+
 # To run the test for features here is kind of experimental
 # they could fail if GitHub blocks external connections.
 # It seems like they rarely do.
