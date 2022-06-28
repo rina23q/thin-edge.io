986956921ab4705949ed031cc1793a7d6ffef734
Rina Fujino <18257209+rina23q@users.noreply.github.com>
2022-06-28 20:47:08 +0200

    bash script improvement
    Signed-off-by: Rina Fujino <18257209+rina23q@users.noreply.github.com>

--- ci/build/build_for_amd64.sh
+++ ci/build/build_for_amd64.sh
@@ -2,20 +2,18 @@
 
 set -euo pipefail
 
-# Use market place actions to enable cache instead of those.
-# If you want to have the same condition as workflow, uncomment lines below.
-# rustup toolchain install 1.58.1 --allow-downgrade --component clippy rustfmt
-# rustup override set 1.58.1
-
+# Install required cargo crates
 cargo install cargo-deb --version 1.38.1
 
-cargo deb -p tedge
-cargo deb -p tedge_mapper
-cargo deb -p tedge_apt_plugin
-cargo deb -p tedge_apama_plugin
-cargo deb -p tedge_agent
-cargo deb -p tedge_watchdog
-cargo deb -p c8y_log_plugin
-cargo deb -p c8y_configuration_plugin
+# Load the release package list as $RELEASE_PACKAGES
+source ./../release_package_list.sh
+
+# Build release debian packages
+for PACKAGE in "${RELEASE_PACKAGES[@]}"
+do
+    cargo deb -p "$PACKAGE"
+done
+
+# Build binaries required by test
 cargo build --release -p sawtooth_publisher
 cargo build --release -p tedge_dummy_plugin
---
+++ ci/release_package_list.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+RELEASE_PACKAGES=(
+tedge
+tedge_mapper
+tedge_agent
+tedge_watchdog
+tedge_apt_plugin
+tedge_apama_plugin
+c8y_log_plugin
+c8y_configuration_plugin
+)
