0c994d13c77717df724f2206db5f591eb7b296f7
Rina Fujino <18257209+rina23q@users.noreply.github.com>
2022-06-29 12:50:06 +0200

    Debug
    Signed-off-by: Rina Fujino <18257209+rina23q@users.noreply.github.com>

--- ci/integration-test/cargo_test_for_amd64.sh
+++ ci/integration-test/cargo_test_for_amd64.sh
@@ -6,6 +6,3 @@ set -euo pipefail
 # this seems to have an impact on some tests as the timing differs
 cargo test --verbose --no-run --features integration-test
 cargo build -p tedge_dummy_plugin
-
-/home/runner/.cargo/bin/cargo test --verbose --features integration-test,requires-sudo -- \
-      --skip sending_and_receiving_a_message
