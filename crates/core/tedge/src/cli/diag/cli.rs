12f9b53da7b8d312a10ac47a46c931351b20c99a
Rina Fujino <rina.fujino.23@gmail.com>
2025-05-08 13:31:57 +0000

    progress commit
    Signed-off-by: Rina Fujino <rina.fujino.23@gmail.com>

--- crates/core/tedge/src/cli/diag/cli.rs
+++ crates/core/tedge/src/cli/diag/cli.rs
@@ -1,11 +1,17 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
 use crate::cli::diag::collect::DiagCollectCommand;
 use crate::command::BuildCommand;
 use crate::command::Command;
 use crate::ConfigError;
+use camino::Utf8Path;
 use camino::Utf8PathBuf;
 use tedge_config::models::SecondsOrHumanTime;
 use tedge_config::TEdgeConfig;
 use tedge_config::TEdgeConfigLocation;
+use time::format_description;
+use time::OffsetDateTime;
 
 #[derive(clap::Subcommand, Debug)]
 pub enum TEdgeDiagCli {
@@ -16,12 +22,14 @@ pub enum TEdgeDiagCli {
         plugin_dir: Utf8PathBuf,
 
         /// Directory where output tarball and temporary output files are stored
+        /// [env: TEDGE_TMP_PATH]
         #[clap(long, default_value = "/tmp")]
         output_dir: Utf8PathBuf,
 
-        /// Filename (without .tar.gz) for the output tarball (default: tedge-diag_<timestamp>)
-        #[clap(long, default_value = "tedge-diag")]
-        tarball_name: String,
+        /// Filename (without .tar.gz) for the output tarball
+        /// [default: tedge-diag_<timestamp>]
+        #[clap(long)]
+        tarball_name: Option<String>,
 
         /// Timeout for each plugin's execution
         #[clap(long, default_value = "10s")]
@@ -41,13 +49,21 @@ impl BuildCommand for TEdgeDiagCli {
                 output_dir,
                 tarball_name,
                 timeout,
-            } => Ok(DiagCollectCommand {
-                plugin_dir,
-                output_dir,
-                tarball_name,
-                timeout,
+            } => {
+                let now = OffsetDateTime::now_utc()
+                    .format(&format_description::well_known::Rfc3339)
+                    .unwrap();
+                let tarball_name = tarball_name.unwrap_or(format!("tedge-diag-{now}"));
+
+                let cmd = DiagCollectCommand {
+                    plugin_dir,
+                    output_dir,
+                    tarball_name,
+                    timeout,
+                }
+                .into_boxed();
+                Ok(cmd)
             }
-            .into_boxed()),
         }
     }
 }
--- crates/core/tedge/src/cli/diag/collect.rs
+++ crates/core/tedge/src/cli/diag/collect.rs
@@ -1,13 +1,17 @@
 use crate::command::Command;
 use crate::log::MaybeFancy;
+use camino::Utf8Path;
 use camino::Utf8PathBuf;
+use std::collections::HashSet;
+use std::os::unix::fs::PermissionsExt;
 use tedge_config::models::SecondsOrHumanTime;
+use tedge_utils::file;
 
 pub struct DiagCollectCommand {
-    pub(crate) plugin_dir: Utf8PathBuf,
-    pub(crate) output_dir: Utf8PathBuf,
-    pub(crate) tarball_name: String,
-    pub(crate) timeout: SecondsOrHumanTime,
+    pub plugin_dir: Utf8PathBuf,
+    pub output_dir: Utf8PathBuf,
+    pub tarball_name: String,
+    pub timeout: SecondsOrHumanTime,
 }
 
 #[async_trait::async_trait]
@@ -17,10 +21,43 @@ impl Command for DiagCollectCommand {
     }
 
     async fn execute(&self) -> Result<(), MaybeFancy<anyhow::Error>> {
-        Ok(collect_diag_info().await?)
+        file::create_directory_with_defaults(&self.output_dir.join(&self.tarball_name))
+            .await
+            .unwrap();
+        let plugins = self.scan_diag_plugins().await?;
+        for plugin in plugins {
+            let filename = plugin.file_name().unwrap();
+            file::create_directory_with_defaults(&self.output_dir.join(&filename))
+                .await
+                .unwrap();
+        }
+
+        Ok(())
     }
 }
 
-async fn collect_diag_info() -> Result<(), anyhow::Error> {
-    unimplemented!()
+impl DiagCollectCommand {
+    async fn scan_diag_plugins(&self) -> Result<HashSet<Utf8PathBuf>, anyhow::Error> {
+        let mut plugins = HashSet::new();
+        let mut entries = tokio::fs::read_dir(&self.plugin_dir).await?;
+
+        while let Some(entry) = entries.next_entry().await? {
+            let path = Utf8PathBuf::from_path_buf(entry.path()).unwrap();
+            if path.is_file() && is_executable(&path).await {
+                plugins.insert(path);
+            }
+        }
+        Ok(plugins)
+    }
+
+    async fn collect_diag_info(plugin_path: &Utf8Path) -> Result<(), anyhow::Error> {
+        unimplemented!()
+    }
+}
+
+async fn is_executable(path: &Utf8Path) -> bool {
+    match tokio::fs::metadata(path).await {
+        Ok(metadata) => metadata.permissions().mode() & 0o111 != 0,
+        Err(_) => false,
+    }
 }
